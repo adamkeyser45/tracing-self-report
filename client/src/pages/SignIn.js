583d3ea5fd2f44a5d9e6669c420f330ac32156b6
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-12-01 22:19:54 -0600

    signIn page remake done

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -2,38 +2,39 @@ import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Link from '@material-ui/core/Link';
+// import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
+// import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import LocalCafeIcon from '@material-ui/icons/LocalCafe';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import Container from '@material-ui/core/Container';
 
 // import { useMutation } from '@apollo/react-hooks';
 // import { LOGIN_USER } from '../../utils/mutations';
 // import Auth from '../../utils/auth';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        javaDript
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="textSecondary" align="center">
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://material-ui.com/">
+//         javaDript
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://image.freepik.com/free-photo/handsome-young-business-man-working-computer-drinking-coffee_1303-21972.jpg)',
+    backgroundImage: 'url(https://indianapublicradio.org/wp-content/uploads/2020/07/contact_tracing-1560x900.jpg)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
@@ -62,7 +63,7 @@ const useStyles = makeStyles((theme) => ({
 export default function SignInSide() {
   const classes = useStyles();
 
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ username: '', password: '' });
   // const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -91,7 +92,7 @@ export default function SignInSide() {
 
     // // clear form values
     // setFormState({
-    //   email: '',
+    //   username: '',
     //   password: '',
     // });
   };
@@ -99,11 +100,21 @@ export default function SignInSide() {
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      <Grid item xs={false} sm={false} md={6} className={classes.image} />
+      <Grid item xs={12} sm={12} md={6} component={Paper} elevation={6} square>
         <div className={classes.paper}>
+
+          <Container maxWidth="lg" component="main" background="main" className={classes.heroContent}>
+            <Typography component="h1" variant="h4" align="center" className="googleFont" gutterBottom>
+              Contact Tracing Self Reporting Form
+            </Typography>
+            <Typography variant="h6" align="center" className="googleFont2" component="p">
+              Please make sure that you sign-in using your specific username and password given to you by your local health department.
+            </Typography>
+          </Container>
+          <br/>
           <Avatar className={classes.avatar}>
-            <LocalCafeIcon />
+            <LocalHospitalIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
@@ -115,12 +126,12 @@ export default function SignInSide() {
             margin="normal"
             required
             fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
             autoFocus
-            value={formState.email}
+            value={formState.username}
             onChange={handleChange}
           />
           <TextField
@@ -147,147 +158,12 @@ export default function SignInSide() {
           </Button>
         </form>
         {/* {error && <div>Login failed</div>} */}
-            <Button
-              type="button"
-              fullWidth
-              variant="contained"
-              color="secondary"
-              className={classes.submit}
-              href="" // http://localhost:3001/auth/github
-            >
-              Coming Soon! Sign In with Github!
-            </Button>
 
-            <Box mt={5}>
+            {/* <Box mt={5}>
               <Copyright />
-            </Box>
+            </Box> */}
         </div>
       </Grid>
     </Grid>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import Avatar from '@material-ui/core/Avatar';
-// import Button from '@material-ui/core/Button';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-// import TextField from '@material-ui/core/TextField';
-// // import FormControlLabel from '@material-ui/core/FormControlLabel';
-// // import Checkbox from '@material-ui/core/Checkbox';
-// import Link from '@material-ui/core/Link';
-// // import Grid from '@material-ui/core/Grid';
-// // import Box from '@material-ui/core/Box';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-// import Typography from '@material-ui/core/Typography';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Container from '@material-ui/core/Container';
-
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="textSecondary" align="center">
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://material-ui.com/">
-//         Your Website
-//       </Link>{' '}
-//       {new Date().getFullYear()}
-//       {'.'}
-//     </Typography>
-//   );
-// }
-
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.secondary.main,
-//   },
-//   form: {
-//     width: '100%', // Fix IE 11 issue.
-//     marginTop: theme.spacing(1),
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 0, 2),
-//   },
-// }));
-
-// export default function SignIn() {
-//   const classes = useStyles();
-
-//   return (
-//     <div className="main">
-//         <br/>
-//     <Container maxWidth="md" component="main" background="main" className={classes.heroContent}>
-//         <Typography component="h1" variant="h3" align="center" className="googleFont" gutterBottom>
-//             Contact Tracing Self Reporting Form
-//         </Typography>
-//         <Typography variant="h5" align="center" className="googleFont2" component="p">
-//             Please make sure that you log-in using your specific username and password given to you by your local health department.
-//         </Typography>
-//     </Container>
-
-//     <Container component="main" maxWidth="xs" className="lightBR">
-//       <CssBaseline />
-//       <div className={classes.paper}>
-//         <Avatar className={classes.avatar}>
-//           <LockOutlinedIcon />
-//         </Avatar>
-//         <Typography component="h1" variant="h5">
-//           Sign in
-//         </Typography>
-//         <form className={classes.form} noValidate>
-//           <TextField
-//             variant="outlined"
-//             margin="normal"
-//             required
-//             fullWidth
-//             id="email"
-//             label="Username"
-//             name="email"
-//             autoComplete="email"
-//             autoFocus
-//           />
-//           <TextField
-//             variant="outlined"
-//             margin="normal"
-//             required
-//             fullWidth
-//             name="password"
-//             label="Password"
-//             type="password"
-//             id="password"
-//             autoComplete="current-password"
-//           />
-//           <Button
-//             type="submit"
-//             fullWidth
-//             variant="contained"
-//             color="primary"
-//             className={classes.submit}
-//           >
-//             Sign In
-//           </Button>
-//         </form>
-//       </div>
-//     </Container>
-//     </div>
-//   );
-// }
