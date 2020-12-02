fbf6e8df09fe35c8dd47ce7cfec7031625b04498
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-12-01 21:54:02 -0600

    nav functioning properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
 
 import './App.css';
-import SignIn from './pages/SignIn';
-import Confidentiality from './pages/Confidentiality';
-import ContactTracing from './pages/ContactTracing';
-import Demographics from './pages/Demographics';
-import Exposure from './pages/Exposure';
-import Symptoms from './pages/Symptoms';
-import Travel from './pages/Travel';
 import Nav from './Nav/index';
 
 
@@ -15,14 +8,7 @@ function App() {
   return (
 
     <div>
-      {/* <SignIn></SignIn> */}
-      {/* <Confidentiality></Confidentiality> */}
-      {/* <ContactTracing></ContactTracing> */}
-      {/* <Demographics></Demographics> */}
-      {/* <Exposure></Exposure> */}
-      {/* <Symptoms></Symptoms> */}
-      <Travel></Travel>
-      {/* <Nav></Nav> */}
+      <Nav></Nav>
     </div>
 
 
--- client/src/Nav/index.js
+++ client/src/Nav/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -7,18 +7,14 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="textSecondary" align="center">
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://material-ui.com/">
-//         Your Website
-//       </Link>{' '}
-//       {new Date().getFullYear()}
-//       {'.'}
-//     </Typography>
-//   );
-// }
+
+import Confidentiality from '../pages/Confidentiality';
+import ContactTracing from '../pages/ContactTracing';
+import Demographics from '../pages/Demographics';
+import Exposure from '../pages/Exposure';
+import SignIn from '../pages/SignIn';
+import Symptoms from '../pages/Symptoms';
+import Travel from '../pages/Travel';
 
 const useStyles = makeStyles((theme) => ({
   '@global': {
@@ -48,6 +44,27 @@ const useStyles = makeStyles((theme) => ({
 export default function Pricing() {
   const classes = useStyles();
 
+  const [currentPage, setCurrentPage] = useState('SignIn');
+
+  const renderPage = () => {
+    switch (currentPage) {
+        case 'Confidentiality':
+          return <Confidentiality />;
+        case 'Demographics':
+            return <Demographics />
+        case 'Symptoms':
+          return <Symptoms />
+        case 'Exposure':
+          return <Exposure />
+        case 'Travel':
+          return <Travel />
+        case 'ContactTracing':
+          return <ContactTracing />
+        default:
+          return <SignIn />;
+    }
+};
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -57,22 +74,22 @@ export default function Pricing() {
             Contact Tracing
           </Typography>
           <nav>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+            <Link variant="button" color="textPrimary" href="#" className={classes.link} onClick={() => { setCurrentPage('Confidentiality') }}>
               Confidentiallity
             </Link>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+            <Link variant="button" color="textPrimary" href="#" className={classes.link} onClick={() => { setCurrentPage('Demographics') }}>
               Demographics
             </Link>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+            <Link variant="button" color="textPrimary" href="#" className={classes.link} onClick={() => { setCurrentPage('Symptoms') }}>
               Symptoms
             </Link>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+            <Link variant="button" color="textPrimary" href="#" className={classes.link} onClick={() => { setCurrentPage('Exposure') }}>
               Exposure
             </Link>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+            <Link variant="button" color="textPrimary" href="#" className={classes.link} onClick={() => { setCurrentPage('Travel') }}>
               Travel
             </Link>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+            <Link variant="button" color="textPrimary" href="#" className={classes.link} onClick={() => { setCurrentPage('ContactTracing') }}>
               Contact Tracing
             </Link>
           </nav>
@@ -81,7 +98,7 @@ export default function Pricing() {
           </Button>
         </Toolbar>
       </AppBar>
-
+      {renderPage()}
     </React.Fragment>
   );
 }
