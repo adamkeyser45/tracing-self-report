c67e68b82f604e43d52e35a9842192ac003e48a4
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-12-03 10:10:23 -0600

    changed tab icon and text

--- client/src/Nav/index.js
+++ client/src/Nav/index.js
@@ -69,7 +69,7 @@ export default function Pricing() {
       <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
           <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-            Contact Tracing
+            Contact Tracing Self Report
           </Typography>
           <nav>
             <Link variant="button" color="textPrimary" href="#" className={classes.link} onClick={() => { setCurrentPage('Confidentiality') }}>
